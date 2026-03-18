df144ece6677addbde022df6bb9d29635b9721e1
alejanr2 <alejanr2@student.42urduliz.com>
2026-03-19 00:46:40 +0100

    un poco de maquetacion al login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -8,24 +8,24 @@ function Login() {
   };
 
   return (
-    <div className="flex align-items-center justify-content-center min-h-screen bg-primary">
-      <div className="w-full px-3 md:px-0 md:w-30rem">
-        <Card className="shadow-8">
-          <div className="text-center mb-5">
-            <h1 className="text-900 text-4xl font-semibold mb-2">Transcendence</h1>
-            <p className="text-600">Inicia sesión con tu cuenta de 42</p>
-          </div>
+    <div className='container-login'>
+      <div className="flex align-items-center justify-content-center min-h-screen bg-primary">
+        <div className="w-full px-3 md:px-0 md:w-30rem">
+          <Card className="shadow-8">
+            <div className="text-center mb-5">
+              <h1 className="text-900 text-4xl font-semibold mb-2">Transcendence</h1>
+            </div>
 
-          <div className="flex flex-column gap-4">
-            <Button
-              label="Iniciar sesión con 42"
-              icon="pi pi-sign-in"
-              className="w-full"
-              size="large"
-              onClick={handleOAuthLogin}
-            />
-          </div>
-        </Card>
+            <div className="flex flex-column gap-4">
+              <Button
+                label="SIGN IN WITH 42"
+                className="w-full"
+                size="large"
+                onClick={handleOAuthLogin}
+              />
+            </div>
+          </Card>
+        </div>
       </div>
     </div>
   )
